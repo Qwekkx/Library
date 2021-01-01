cf9f8c11f798e35891199fbe6feb46e362b81fcf
Adamska171008 <2639980868@qq.com>
2021-01-02 00:19:02 +0800

    BookWare二叉搜索树基本写完

--- code/Book.h
+++ code/Book.h
@@ -13,12 +13,20 @@ public:
 
     Book() : ID(0), lended(false), left(NULL), right(NULL) {}
     Book(string a, string b, string c, string d) : name(a), ISBN(b), author(c), type(d) {}
+    void printInfo();
+    bool operator<(Book anthor) { return this->name < anthor.name; }
 };
 
 struct bookNode
 {
     bookNode() : left(NULL), right(NULL) {}
     bookNode(Book book) : data(book), left(NULL), right(NULL) {}
+    void printInfo();
     Book data;
     bookNode *left, *right;
 };
+
+void Book::printInfo()
+{
+    cout << "name: " << name << " ISBN: " << ISBN << " author: " << author << " type: " << type << endl;
+}
--- code/BookWare.h
+++ code/BookWare.h
@@ -2,6 +2,7 @@
 #include "Book.h"
 #include <map>
 #include <vector>
+#include <algorithm>
 
 #pragma once
 using namespace std;
@@ -11,6 +12,7 @@ class BookWare
 private:
     bookNode *root;
     map<string, string> name_to_ISBN;
+    vector<Book> printList;
 
 public:
     BookWare() : root(NULL), capacity(0) {}
@@ -18,10 +20,10 @@ public:
     void Library::insert(Book *book, bookNode *father);
     bookNode *SearchByName(string dst, bookNode *father = root);
     bookNode *SearchByISBN(string dst, bookNode *father = root);
-    void SearchByAuthor(string vague, bookNode *father = root);
+    void SearchByAuthor(string vague);
+    void SearchByAuthor(string vague, bookNode *father);
     void SearchByType(string vague) {}
     void *deleteBook(string book) {}
-    void printInf(Book *book) {}
 };
 
 bookNode *BookWare::SearchByName(string dst, bookNode *father)
@@ -43,11 +45,42 @@ bookNode *BookWare::SearchByType(string dst, bookNode *father)
     return SearchByName(it_find->second)
 }
 
-void BookWare::SearchByAuthor(string vague, bookNode *father = root)
+void BookWare::SearchByAuthor(string vague)
+{
+    SearchByAuthor(vague, root);
+    sort(printlist.begin(), printList.end());
+    for (int i = 0; i < printList.size(), i++)
+        printList[i].printInfo();
+    printList.clear();
+}
+
+void BookWare::SearchByAuthor(string vague, bookNode *father)
 {
     if (father = NULL)
         return;
-    else if ()
+    else if (father->data.author == vague)
+        printList.push_back(father->data);
+    SearchByAuthor(vague, father->left);
+    SearchByAuthor(vague, father->right);
+}
+
+void BookWare::SearchByType(string vague)
+{
+    SearchByType(vague, root);
+    sort(printlist.begin(), printList.end());
+    for (int i = 0; i < printList.size(), i++)
+        printList[i].printInfo();
+    printList.clear();
+}
+
+void BookWare::SearchByType(string vague, bookNode *father)
+{
+    if (father = NULL)
+        return;
+    else if (father->data.type == vague)
+        printList.push_back(father->data);
+    SearchByType(vague, father->left);
+    SearchByType(vague, father->right);
 }
 
 void BookWare::insert(Book *book)
